bb3b4fa97fa8c60dcb581f027501ee04c6d8bd2c
Muhammad Ilyas <m.ilyasalfadlih@gmail.com>
2025-04-27 16:17:18 +0700

    Sepatu

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const db = mysql.createConnection({
 
 db.connect(err => {
   if (err) {
-    console.error('Gagal koneksi ke database HAHAHA gblk:', err);
+    console.error('Gagal koneksi ke database:', err);
   } else {
     console.log('Terhubung ke database MySQL RDS');
   }
